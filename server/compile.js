b4ea0848f2c1de4d859886e0b94b5a925e84622a
piyush mehta <me@piyushmehta.com>
2018-10-07 03:23:40 +0530

    Updates: Added compile scripts

--- server/compile.js
+++ server/compile.js
@@ -8,12 +8,11 @@ fs.removeSync(buildPath);
 const campaignPath = path.resolve(__dirname, 'contracts', 'Chat.sol');
 const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;
-
 fs.ensureDirSync(buildPath);
 
 for (let contract in output) {
   fs.outputJsonSync(
-    path.resolve(buildPath, contract.replace(':', '') + '.json'),
+    path.resolve(buildPath, 'Chat' + '.json'),
     output[contract]
   );
 }
