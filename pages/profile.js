0302bc69c4575ea74fe03b2f6a4e6ddc0d47d261
piyush mehta <me@piyushmehta.com>
2018-10-07 01:19:01 +0530

    server
    Signed-off-by: piyush mehta <me@piyushmehta.com>

--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react'
 import { Avatar } from 'antd';
 import { Row, Col } from 'antd';
+import { Checkbox } from 'antd';
 
 class Profile extends Component{
+  onChange(e) {
+    console.log(`checked = ${e.target.checked}`);
+  }
   render() {
     return (
       <div>
@@ -12,7 +16,8 @@ class Profile extends Component{
           </Col>
           <Col xs={{ span: 12, offset: 6 }} lg={{ span: 21, offset: 0 }} style={{paddingTop:"20px"}}>
             <h1 style={{textAlign: "center"}}>Aditya Gupta</h1>
-            <h2 style={{textAlign: "center"}}>Student</h2>
+            <h2 style={{textAlign: "center"}}>Bounty setter?</h2>
+            <Checkbox onChange={this.onChange}>Checkbox</Checkbox>
           </Col>
         </Row>
       </div>
