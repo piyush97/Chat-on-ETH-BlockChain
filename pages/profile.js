6f2ef02b949f48b2f2b7201b314d7c5a1d20b716
piyush mehta <me@piyushmehta.com>
2018-10-07 01:33:30 +0530

    profile bounty setter
    Signed-off-by: piyush mehta <me@piyushmehta.com>

--- pages/profile.js
+++ pages/profile.js
@@ -1,23 +1,49 @@
-import React, {Component} from 'react'
-import { Avatar } from 'antd';
-import { Row, Col } from 'antd';
-import { Checkbox } from 'antd';
+import React, { Component } from "react";
+import { Avatar } from "antd";
+import { Row, Col } from "antd";
+import { Checkbox } from "antd";
+import { Slider, InputNumber } from "antd";
 
-class Profile extends Component{
-  onChange(e) {
-    console.log(`checked = ${e.target.checked}`);
+class Profile extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      BountySetter: false,
+      inputValue: 1
+    };
   }
+  onChange = (e, value) => {
+    this.setState({ BountySetter: e.target.checked });
+    this.setState({ inputValue: value });
+  };
   render() {
+    const { inputValue } = this.state;
     return (
       <div>
         <Row>
-          <Col xs={{ span: 12, offset: 6 }} lg={{ span: 22, offset: 10 }} style={{paddingTop:"20px"}}>
+          <Col
+            xs={{ span: 12, offset: 6 }}
+            lg={{ span: 22, offset: 10 }}
+            style={{ paddingTop: "20px" }}
+          >
             <Avatar size={64} icon="user" />
           </Col>
-          <Col xs={{ span: 12, offset: 6 }} lg={{ span: 21, offset: 0 }} style={{paddingTop:"20px"}}>
-            <h1 style={{textAlign: "center"}}>Aditya Gupta</h1>
-            <h2 style={{textAlign: "center"}}>Bounty setter?</h2>
+          <Col
+            xs={{ span: 12, offset: 6 }}
+            lg={{ span: 21, offset: 0 }}
+            style={{ paddingTop: "20px" }}
+          >
+            <h1 style={{ textAlign: "center" }}>Aditya Gupta</h1>
+            <h2 style={{ textAlign: "center" }}>Bounty setter?</h2>
             <Checkbox onChange={this.onChange}>Checkbox</Checkbox>
+            {this.state.BountySetter && (
+              <Slider
+                min={1}
+                max={20}
+                onChange={this.onChange}
+                value={typeof inputValue === "number" ? inputValue : 0}
+              />
+            )}
           </Col>
         </Row>
       </div>
